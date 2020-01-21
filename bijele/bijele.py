39124bfa1641366db58426d5be47e044e648a658
fh-vh <60002745+fh-vh@users.noreply.github.com>
2020-01-21 11:39:24 +0100

    Update bijele.py

--- bijele/bijele.py
+++ bijele/bijele.py
@@ -3,7 +3,7 @@
 # Python solution to "Bijele" problem on kattis.com (https://open.kattis.com/problems/bijele)
 # By Frederik Hoengaard - @frederikhoengaard - frph@itu.dk
 
-# In progress
+# Is tested and works as of 21 Jan 2020
 
 #====================================================================================================
 #====================================================================================================
@@ -11,16 +11,24 @@
 
 import sys
 
-x = sys.stdin.read()
+input_data = sys.stdin.read()
 
-y = [int(i) for i in x.split(' ')]
+int_list = [int(i) for i in input_data.split(' ')]
+valid_list = [1,1,2,2,2,8] 
 
-
+def check_valid(int_list,valid_list):
+    if not int_list:
+        return ''
+    else:
+        if len(int_list) == 1:
+            return str(valid_list[0] - int_list[0])
+        else:
+            return str(valid_list[0] - int_list[0]) + ' ' + check_valid(int_list[1:],valid_list[1:]) 
 
 #====================================================================================================
 
 def main():
-	pass
+	print(check_valid(int_list,valid_list))
 
 if __name__ == '__main__':
 	main()
